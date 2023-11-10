20a856056f8be508de4d1984710426f4a42447f2
DEEPAK-SINGH-GOOR <deepaksinghgoor745@gmail.com>
2023-11-10 19:54:20 +0530

    PR-2

---
+++ PR-2/D-C6-PR2-1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+main()
+{
+  int n;
+
+  printf("Enter any value :");
+  scanf("%d",&n);
+  (n%2==0)
+	 ?printf("This value is Even!!")
+	 :printf("This value is Odd!!");
+
+
+
+}
---
+++ PR-2/D-C6-PR2-2.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+   
+     main ()
+     {
+     int n ;
+     float a,b,c,d ;
+   
+     printf (" enter your unit : " ) ;
+     scanf ("%d",&n) ; 
+    if (n>=1 && n<=50)
+    {
+        a=n*0.5;
+        printf("Your Electric Bill is %.2f",(a*0.2)+a);
+    } 
+    
+   else if (n>=51 && n<=150)
+    {
+        b=((n-50)*0.75+25);
+        printf("Your  Electric Bill is %.2f",(b*0.2)+b);
+    } 
+    else if(n>151 && n<=250)
+    {
+        c=((n-151)*1.20+50);
+        printf("Your  Electric Bill is %.2f",(c*0.2)+c);
+    }
+    
+    else if(n>=250)
+    {
+        d=((n-250)*1.50+220);
+        printf("Your  Electric Bill is %.2f",(d*0.2)+d);
+    }
+    else
+    {
+       printf("YOUR  Electric BILL UNIT IS INVALIDE !!");
+    }
+}
