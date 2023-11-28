5534b6ffd354f47a649557c91445b772e4a19609
DEEPAK-SINGH-GOOR <deepaksinghgoor745@gmail.com>
2023-11-28 20:04:45 +0530

    7.1-1

---
+++ 7.1-1/7.1-7.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+main()
+{ 
+   
+   char i='A',j='E';
+      for(i='A';i<='E';i++)
+  {
+      for(j=i;j>='A';j--)
+    {
+          printf("%c ",j);   
+    }
+    printf("\n");
+  }
+  
+
+}
