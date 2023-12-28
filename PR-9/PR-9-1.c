ae7c56b573a765007532bac4cd463de1cd01f3c9
DEEPAK-SINGH-GOOR <deepaksinghgoor745@gmail.com>
2023-12-28 16:15:59 +0530

    PR-9

--- PR-9/PR-9-1.c
+++ PR-9/PR-9-1.c
@@ -6,7 +6,7 @@ void main()
    	char string[20];
    	int *ptr;
    	ptr=&string;
-   	
+
 	printf("Enter Any Name :");	
     gets(string);
     
--- PR-9/PR-9-2.c
+++ PR-9/PR-9-2.c
@@ -1,26 +1,10 @@
 #include<stdio.h>
-void cube(int r, int c,int *ptr[r][c])
-{
-	int i,j;
-	for(i=0;i<r;i++)
-	{
-		for(j=0;j<c;j++)
-		{
-			printf("%d\n",(*ptr[i][j])*(*ptr[i][j])*(*ptr[i][j]));	
-		}
-	}
-}
-	
-
-
 void main()
 {
 	int r, c; 
 
-
 	printf("Enter Row size :");
 	scanf("%d",&r);
-	
 	printf("Enter Column size :");
 	scanf("%d",&c);
 
@@ -33,7 +17,6 @@ void main()
 		printf("a[%d][%d]=",i,j);
 		scanf("%d",&arr[i][j]);
 	    }
-	    
 	}
 	printf("===============\n");
 	printf("Cube of Array :\n");
@@ -45,9 +28,17 @@ void main()
     		ptr[i][j]=&arr[i][j];
 		}
 	}
-	
 	cube(r,c,&ptr);
 }
 
-
-
+void cube(int r, int c,int *ptr[r][c])
+{
+	int i,j;
+	for(i=0;i<r;i++)
+	{
+		for(j=0;j<c;j++)
+		{
+			printf("%d\n",(*ptr[i][j])*(*ptr[i][j])*(*ptr[i][j]));	
+		}
+	}
+}
